932455c0f5ed056a9b65d74f6b7819e51144ed64
Ryan Maloney <rypmaloney@gmail.com>
2021-11-16 16:16:34 -0500

    refactored to not use separate objects in state. This allows me to pass just the state to each component. I can also then handlechange using the id of the form field to select the correct key to update in state.

--- src/App.js
+++ src/App.js
@@ -9,54 +9,45 @@ class App extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            contact: {
-                name: "Ryan Maloney",
-                address: "Boston, MA",
-                email: "rypmaloney@gmail.com",
-            },
+            name: "Ryan Maloney",
+            address: "Boston, MA",
+            email: "rypmaloney@gmail.com",
         };
     }
 
     onSubmitName = (e) => {
         e.preventDefault();
         this.setState({
-            contact: {
-                name: e.target.name.value,
-                address: e.target.address.value,
-                email: e.target.email.value,
-            },
+            name: e.target.name.value,
+            address: e.target.address.value,
+            email: e.target.email.value,
         });
     };
 
     handleChange = (e) => {
-      console.log(e.target.id)
-      let field = e.target.id
-      this.setState({
-        contact: {
-          [field]: e.target.value,
-
-      },
-      });
-
-    }
+        console.log(e.target.id);
+        let field = e.target.id;
+        this.setState({
+            [field]: e.target.value,
+        });
+    };
 
     render() {
         return (
             <div className="App">
-            
-              <div className="form">
-              <h1>Resume Builder</h1>
-                <Contact
-                    change={this.handleChange}
-                    click={this.onSubmitName}
-                    contact={this.state.contact}
-                />
-                <Education />
-                <Experience />
-              </div>
-              <div className="display">
-                <Display contact={this.state.contact} />
-              </div>
+                <div className="form">
+                    <h1>Resume Builder</h1>
+                    <Contact
+                        change={this.handleChange}
+                        click={this.onSubmitName}
+                        contact={this.state.contact}
+                    />
+                    <Education />
+                    <Experience />
+                </div>
+                <div className="display">
+                    <Display contact={this.state} />
+                </div>
             </div>
         );
     }
